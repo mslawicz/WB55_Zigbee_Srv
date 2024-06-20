671abd66e283ae20a0386dbec5b8054780fdaea3
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-06-20 17:41:12 +0200

    Zigbee server manufacturer and model name set

--- STM32_WPAN/App/app_zigbee.c
+++ STM32_WPAN/App/app_zigbee.c
@@ -41,6 +41,7 @@
 /* USER CODE BEGIN Includes */
 #include "main.h"
 #include "rgb_driver.h"
+#include "zcl.basic.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -141,6 +142,8 @@ static struct ZbZclOnOffServerCallbacksT OnOffServerCallbacks_1 =
 };
 
 /* USER CODE BEGIN PV */
+uint8_t manufacturerName[] = "_Cinek2030";
+uint8_t modelName[] = "_WS2812A controller";
 /* USER CODE END PV */
 /* Functions Definition ------------------------------------------------------*/
 
@@ -342,6 +345,10 @@ static void APP_ZIGBEE_StackLayersInit(void)
   APP_ZIGBEE_ConfigEndpoints();
 
   /* USER CODE BEGIN APP_ZIGBEE_StackLayersInit */
+  manufacturerName[0] = strlen(manufacturerName) - 1;
+  ZbZclBasicWriteDirect(zigbee_app_info.zb, SW1_ENDPOINT, ZCL_BASIC_ATTR_MFR_NAME, manufacturerName, manufacturerName[0] + 1);
+  modelName[0] = strlen(modelName) - 1;
+  ZbZclBasicWriteDirect(zigbee_app_info.zb, SW1_ENDPOINT, ZCL_BASIC_ATTR_MODEL_NAME, modelName, modelName[0] + 1);
   /* USER CODE END APP_ZIGBEE_StackLayersInit */
 
   /* Configure the joining parameters */
