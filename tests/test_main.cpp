7bd8d61f652fc3532e703bf65a311a6428975b3a
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-06-24 14:54:28 +0200

    google test added; errors

---
+++ tests/test_main.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "stm32wbxx_hal.h"
+
+TEST(HALTest, Initialization) {
+    HAL_StatusTypeDef status = HAL_Init();
+    ASSERT_EQ(status, HAL_OK);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
