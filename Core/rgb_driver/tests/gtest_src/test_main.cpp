34c65bfb09f196cdaeebbe755e0a81ee915f6884
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-06-28 09:27:31 +0200

    test of convert_xy_to_RGB

--- Core/rgb_driver/tests/gtest_src/test_main.cpp
+++ Core/rgb_driver/tests/gtest_src/test_main.cpp
@@ -7,13 +7,25 @@ extern "C" {
 #include "rgb_driver.h"
 }
 
-TEST(convert_xy_to_RGB, not_zero) {
-    // Call the function to be tested
-    RGB rgb = convert_xy_to_RGB(XY{10000, 15000});
-    EXPECT_NE(rgb.B, 0);
-    // Assert expected behavior
-    // Here you would check if the mocked HAL function was called as expected
-    // This might involve setting flags or using a mock framework to verify calls
+TEST(convert_xy_to_RGB, color_white) {
+    RGB rgb = convert_xy_to_RGB(XY{21167, 21561});
+    EXPECT_GT(rgb.R, 250);
+    EXPECT_GT(rgb.G, 250);
+    EXPECT_GT(rgb.B, 250);
+}
+
+TEST(convert_xy_to_RGB, color_red) {
+    RGB rgb = convert_xy_to_RGB(XY{45808, 19594});
+    EXPECT_GT(rgb.R, 250);
+    EXPECT_LT(rgb.G, 20);
+    EXPECT_LT(rgb.B, 20);
+}
+
+TEST(convert_xy_to_RGB, color_blue) {
+    RGB rgb = convert_xy_to_RGB(XY{8912, 2621});
+    EXPECT_LT(rgb.R, 20);
+    EXPECT_LT(rgb.G, 20);
+    EXPECT_GT(rgb.B, 250);
 }
 
 int main(int argc, char **argv) {
